0166ed5621b4f027aebc57656ada1af32a1bd9c7
Roman Mayer <romanmayer1303@gmail.com>
2017-02-20 23:20:02 +0100

    added extra credit functionality

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,60 +1,35 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Game {
     public static void main(String[] args) {
 
-        System.out.println("ADMINISTRATOR SETUP");
-        System.out.println("===================");
+        Prompter prompter = new Prompter();
 
-        String itemName;
-        int maxNumberOfItems;
+        String itemName = prompter.askForItemName();
+        int maxNumberOfItems = prompter.askForMaxNumberOfItems(itemName);
+        String name = prompter.askForUsername();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        try {
-            System.out.print("Name of items in the jar: ");
-            itemName = br.readLine().replaceAll("\\s", "");
-            System.out.printf("Maximum number of %s in the jar: ", itemName);
-            maxNumberOfItems = Integer.parseInt(br.readLine().replaceAll("\\s", ""));
+        Jar jar = new Jar(itemName, maxNumberOfItems);
 
+        prompter.promptToStartTheGame(itemName, maxNumberOfItems);
 
-
-            System.out.println("PLAYER");
-            System.out.println("===================");
-            System.out.printf("Your goal is to guess how many %s are in the jar. ", itemName);
-            System.out.printf("Your guess should be between 1 and %s.%n", maxNumberOfItems);
-            System.out.println();
-            System.out.println("Ready? Press ENTER to start guessing!");
-
-            String enter = " ";
-            do {
-                enter = br.readLine();
-                System.out.println(enter);
-            } while (enter != null && !enter.equals(""));
-            Jar jar = new Jar(itemName, maxNumberOfItems);
-
-            int counter = 0;
-            int guess = -1;
-            while (guess != jar.getNumberOfItems()) {
-                System.out.print("Guess: ");
-                String input = br.readLine();
-                if (input != null) {
-                    guess = Integer.parseInt(input.replaceAll("\\s", ""));
-//                guess = Integer.parseInt(br.readLine().replaceAll("\\s", ""));
-                    System.out.println("yolo: " + Integer.toString(guess));
-                    counter++;
-                } else {
-                    System.out.println("Input was null.");
+        int counter = 0;
+        int guess = -1;
+        while (guess != jar.getNumberOfItems()) {
+            guess = prompter.promptForGuess();
+            if (guess > 0 && guess <= maxNumberOfItems) {
+                counter++;
+                if (guess < jar.getNumberOfItems()) {
+                    prompter.guessIsTooLow();
+                } else if (guess > jar.getNumberOfItems()) {
+                    prompter.guessIsTooHigh();
                 }
+            } else {
+                prompter.outOfRange(maxNumberOfItems);
             }
-            System.out.printf("Congratulations, %s is the correct answer.%n", guess);
-            System.out.printf("There are %s %s in the jar.%n", guess, jar.getItemName());
-            System.out.printf("It took you %s guesses.%n", counter);
-
-        } catch (IOException ioe) {
-            System.out.println(ioe.getMessage());
         }
+
+        prompter.printResult(counter);
 
     }
 }
